b024602b1ed78d2eee7b67d9c5a0fcea367ddc8c
Mauricio-Garcia-A <garcia.mauricio.ariel@gmail.com>
2023-07-18 05:37:16 -0300

    Maquetado App - v0.01

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import './App.css'
+import Home from './pages/Home'
 
-function App() {
+const listOfCities = [
+  'San Carlos de Bariloche',
+  'Villa Carlos Paz',
+  'Cafayate',
+  'Ushuaia',
+  'Puerto Iguazú'
+]
+
+function App (): JSX.Element {
   return (
     <>
-      Hola mundo
+      <header>
+        Encabezado de la APP
+      </header>
+      <main>
+        <Home />
+      </main>
+      <footer>
+        Pie de Pagina de la APP
+      </footer>
     </>
   )
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+export default function Home (): React.FC {
+  return (
+    <section>
+      <header>
+        Selector de Ciudades
+      </header>
+      <main>
+        Informacion Clima (ciudad actual)
+      </main>
+      <footer>
+        Pronostico extendido (5 dias)
+      </footer>
+    </section>
+  )
+}
